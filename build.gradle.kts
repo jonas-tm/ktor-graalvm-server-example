7043e8368b76e5c586da6efb9be5ec937797bf3f
jonas-tm <ext-jonas.martini@goflink.com>
2023-01-04 18:15:00 +0100

    Add ktor plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,21 @@
-val ktor_version =  "2.1.1"
-val kotlin_version =  "1.7.10"
-val logback_version =  "1.4.1"
-val exposed_version =  "0.39.2"
-val h2_version =  "2.1.214"
+val kotlin_version = "1.8.0"
+val logback_version = "1.4.1"
+val exposed_version = "0.39.2"
+val h2_version = "2.1.214"
 val prometheus_version = "1.9.4"
 
 plugins {
     application
-    kotlin("jvm") version "1.7.10"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.7.10"
-    id("org.graalvm.buildtools.native") version "0.9.14"
-    id("com.github.johnrengelman.shadow") version "7.1.1"
+    kotlin("jvm") version "1.8.0"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.0"
+    id("io.ktor.plugin") version "2.2.2"
+    id("org.graalvm.buildtools.native") version "0.9.19"
+//    id("com.github.johnrengelman.shadow") version "7.1.1"
 }
 
 group = "com.example"
 version = "0.0.1"
+
 application {
     mainClass.set("com.example.ApplicationKt")
 
@@ -30,17 +31,17 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
 
-    implementation("io.ktor:ktor-server-compression-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-cio:$ktor_version")
+    implementation("io.ktor:ktor-server-compression-jvm")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm")
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-cio")
 
-    implementation("io.ktor:ktor-server-status-pages:$ktor_version")
-    implementation("io.ktor:ktor-server-call-id:$ktor_version")
+    implementation("io.ktor:ktor-server-status-pages")
+    implementation("io.ktor:ktor-server-call-id")
 
-    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json")
 
-    implementation("io.ktor:ktor-server-metrics-micrometer:$ktor_version")
+    implementation("io.ktor:ktor-server-metrics-micrometer")
     implementation("io.micrometer:micrometer-registry-prometheus:$prometheus_version")
 
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
@@ -50,10 +51,27 @@ dependencies {
 
     implementation("ch.qos.logback:logback-classic:$logback_version")
 
-    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
+    testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
-    testImplementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
+    testImplementation("io.ktor:ktor-client-content-negotiation")
 }
+
+ktor {
+    docker {
+        jreVersion.set(io.ktor.plugin.features.JreVersion.JRE_17)
+        localImageName.set("ktor-graalvm-server-example")
+        imageTag.set("my-docker-sample")
+
+        externalRegistry.set(
+            io.ktor.plugin.features.DockerImageRegistry.dockerHub(
+                appName = provider { "ktor-graalvm-server-example" },
+                username = providers.environmentVariable("DOCKER_USERNAME"),
+                password = providers.environmentVariable("DOCKER_PASSWORD")
+            )
+        )
+    }
+}
+
 
 graalvmNative {
     metadataRepository {
